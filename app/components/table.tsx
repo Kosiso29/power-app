527120b543a53289107c2cbdeb05922e1f31c3e3
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-02-24 19:46:55 +0100

    24/2/24: add loading icon

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -6,6 +6,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from "next/link";
 import Appliance from "./appliance";
+import Loading from "../ui/loading";
 
 const daysMap = {
     "Monday": "M",
@@ -156,6 +157,11 @@ export default function Table({ schedules }) {
                             ))}
                         </tbody>
                     </table>
+                </div>
+                <div className='mt-8'>
+                    {
+                        schedules.length === 0 && <Loading />
+                    }
                 </div>
             </div>
         </div>
---
+++ app/ui/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div className='w-full'>
+            <div className='b border-[6px] border-t-[6px] border-primary border-t-transparent rounded-full w-16 h-16 animate-spin ml-auto mr-auto'></div>
+        </div>
+    )
+}
