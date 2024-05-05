80a4185ddf2a83fd8e74fe8a430693076aef96b1
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-05-05 15:27:58 +0100

    5/5/24: update placeholder color

--- app/ui/text-input.tsx
+++ app/ui/text-input.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function TextInput({ type, value, onChange, placeholder, className }: { type: string, value: string, onChange: Function, placeholder: string, className: string }) {
     return (
-        <input className={`bg-transparent border-primary border-b-2 px-3 py-2 focus-visible:outline-none placeholder-[#DDDDDD] ${className}`} type={type} value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)} placeholder={placeholder} />
+        <input className={`bg-transparent border-primary border-b-2 px-3 py-2 focus-visible:outline-none ${className}`} type={type} value={value} onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)} placeholder={placeholder} />
     )
 }
