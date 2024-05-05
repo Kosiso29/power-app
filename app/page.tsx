80b4d11711f6ad4edd139d9bbe8611d934022640
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-05-05 23:01:03 +0100

    5/5/24: implement mobile version for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Button from "./ui/button";
 import Link from 'next/link';
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="bg-[url('/powersync-background.jpeg')] bg-center h-screen flex justify-end">
-            <div className="flex flex-col justify-center gap-16 items-center text-center w-1/2 shadow-text">
+        <div className="bg-[#080c1c] lg:bg-[url('/powersync-background.jpeg')] bg-center h-screen lg:flex lg:justify-end">
+            <div className="lg:hidden max-h-[60vh] h-[60vh]">
+                <Image src="/mobile-home-background.jpeg" className="block max-h-full" fill style={{ maxHeight: "60vh" }} objectFit="cover" alt="bg" />
+            </div>
+            <div className="flex flex-col justify-center gap-16 items-center text-center h-[40vh] lg:h-auto lg:w-1/2 shadow-text">
                 <div>
                     <h1 className="text-6xl font-bold mb-10">Powersync</h1>
                     <p className="text-lg">Power well managed is power well used, <br />allow us to manage your power!</p>
